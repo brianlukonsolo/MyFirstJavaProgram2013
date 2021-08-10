3b831538d8009891a81f79ee1cd1c76ef6b7df07
Brian Lukonsolo <brian.lukonsolo@gmail.com>
2021-08-10 23:40:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyFirstJavaProgram2013
+ A C program I created in the year 2013. I uploaded it to remind myself how bad my code was, yet how far I have come and how much I taught myself too!
---
+++ helloworld.java
@@ -0,0 +1,13 @@
+import static java.lang.system.out;
+
+class Paybarry
+{
+	public static void main(string args[])
+	{
+		double checkAmount = 1257.63;
+		out.print("Pay to the order of ");
+		out.print("Dr. Barry Burd ");
+		out.print("$");
+		out.printLn(checkAmount);
+	}
+}
